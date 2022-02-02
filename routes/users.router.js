4d477157415742467d975230f47f807f567a0c57
Fernando Ochoa <Fernando268a@gmail.com>
2022-02-02 13:23:16 -0500

    next users

--- routes/users.router.js
+++ routes/users.router.js
@@ -4,88 +4,100 @@ const userServices  = require("../services/users.services");
 
 const service = new userServices()
 
-router.get("/", async (req,res) => {  //get all users
+router.get("/", async (req,res, next) => {  //get all users
     try {
         const body = req.body;
         const users = await service.find(body);
         res.json(users);
     } catch (error) {
-        res.json({
-            message: error,
-        })
+        next(error);
     }
 })
 
-router.get("/:id", async (req,res)=> {   //send inf. of user
+router.get("/:id", async (req, res, next)=> {   //send inf. of user
     try {
         const { id } = req.params;
         const user = await service.findCount(id);
         res.json(user);
     } catch (error) {
-        //
+        next(error);
     }
 })
 
-router.get("/:id/car", (req,res)=> {  //show the car 
-    const { id } = req.params;
-    const userCar = service.showCar(id);
-    res.json(userCar);
+router.get("/:id/car", (req, res, next)=> {  //show the car 
+    try {
+        const { id } = req.params;
+        const userCar = service.showCar(id);
+        res.json(userCar);
+    } catch (error) {
+        next(error);
+    }
 })
 
-router.get("/:id/order", (req, res)=> {
-    const { id } = req.params;
-    const userOrder = service.showOrders(id);
-    res.json(userOrder);
+router.get("/:id/order", (req, res, next)=> {
+    try {
+        const { id } = req.params;
+        const userOrder = service.showOrders(id);
+        res.json(userOrder);
+    } catch (error) {
+        next(error);
+    }
 })
 
-router.get("/:id/purchased", (req, res)=> {
-    const { id } = req.params;
-    const userPurchased = service.showProductsPurchased(id);
-    res.json(userPurchased);
+router.get("/:id/purchased", (req, res, next)=> {
+    try {
+        const { id } = req.params;
+        const userPurchased = service.showProductsPurchased(id);
+        res.json(userPurchased);
+    } catch (error) {
+        next(error);
+    }
 })
 
 //post
 
-router.post("/", async(req, res) => {
+router.post("/", async(req, res, next) => {
     try {
         const body = req.body;
         const newUser = await service.creatNewUser(body);
         res.json(newUser);
     } catch(error){
-        res.status(400).json(error);
+        next(error);
     }
 })
 
 
-router.post("/:id/car", (req,res)=> {  //add car 
-    const { id } = req.params;
-    const body = req.body;
-    const userCar = service.addCar(id, body);
-    res.json(userCar);
+router.post("/:id/car", (req,res, next)=> {  //add car 
+    try {
+        const { id } = req.params;
+        const body = req.body;
+        const userCar = service.addCar(id, body);
+        res.json(userCar);
+    } catch (error) {
+        next(error);
+    }
 })
 
 
 // update
-router.patch("/:id", async (req, res) => {
+router.patch("/:id", async (req, res, next) => {
     try {
         const { id }= req.params;
         const body = req.body;
         const userUpdate = await service.update(id,body);
         res.json(userUpdate);
     } catch (error) {
-        res.json({
-            message: error
-        });
+        next(error);
     }
 })
 
-router.delete("/", async (req, res) => {
+router.delete("/", async (req, res, next) => {
     try {
         const body = req.body;
         const userDeleted = await service.deleteCount(body);
         res.json(userDeleted);
     } catch (error) {
-        res.json(error);
+        next(error);
     }
 })
 
