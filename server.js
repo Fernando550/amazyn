bf5f6ad26f7a4a6b585e132b0ee4b639f30720f4
Fernando Ochoa <Fernando268a@gmail.com>
2022-02-02 13:15:17 -0500

    middlewares, errors with boom

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app =  express();
 const  port = 3000;
 const routerApi = require("./routes/index");
 const db = require("./connectionDatabase");
+const {logErrors, errorHandler,boomErrorHandler} = require("./middlewares/error.handler");
 
 app.use(express.json()); 
 
 routerApi(app);
 
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(port);
